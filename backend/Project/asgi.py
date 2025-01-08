30ecf0f67d93e1166bb21309fcad87ae3e1846ff
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-01-08 20:23:26 +0530

    Update asgi.py

--- backend/Project/asgi.py
+++ backend/Project/asgi.py
@@ -2,11 +2,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from api.middleware import JWTAuthMiddleware 
 from videochat import routings as video_routing
 # from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+
+# Initialize Django and set up the app registry
+django.setup()  # This ensures that the apps are fully loaded before proceeding
+
+# Now it's safe to import middleware and other Django-dependent modules
+from api.middleware import JWTAuthMiddleware
 
 django_asgi_app = get_asgi_application()
 
