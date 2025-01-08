93b15ac8b447fe2d6d5bc2f73a9693223554cb80
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-01-08 19:41:40 +0530

    Update settings.py

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -123,20 +123,26 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'Project.asgi.application'
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer",
+#     },
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.getenv("RHOST")],
+        },
     },
 }
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+    "default":dj_database_url.parse(os.getenv('HOST'))
 }
 
 # Password validation
