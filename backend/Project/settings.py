719ac264b2fa407cef5c5c075eaf4bb26d8b916c
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-01-08 19:45:47 +0530

    Update settings.py

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -259,10 +259,10 @@ TEMP = os.path.join(BASE_DIR , 'media_cdn/temp')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # mail
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'notoherupa@gmail.com'
-EMAIL_HOST_PASSWORD = 'rnuuniecqypgvdvc'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
