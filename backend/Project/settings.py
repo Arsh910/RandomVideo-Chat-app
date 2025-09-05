166dd83747cb60a05bc03b91c54582d11fb8175d
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2025-09-05 14:36:30 +0530

    Update settings.py

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -239,7 +240,6 @@ AUTHENTICATION_BACKENDS = (
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
-    os.path.join(BASE_DIR,'media'),
 ] 
 
 STATIC_URL = '/static/'
@@ -265,4 +265,3 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
